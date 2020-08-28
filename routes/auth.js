7713dfb516be5d8c86a4e81f3afd04f6ccf39dee
suvechhaya <suvechhaya@bajratechnologies.com>
2020-08-28 12:35:08 +0545

    Create User model and add validation schema in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-  res.send('Register');
-})
+//Validation
+const Joi = require('@hapi/joi');
 
+const schema = {
+  name: Joi.string().required(),
+  email: Joi.string().required().email(),
+  password: Joi.string().min(8).required()
+};
+
+router.post('/register', async (req, res) => {
+
+  //Validating the data before we make a user
+  const validation = Joi.validate(req.body, schema);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const saveUser = await user.save();
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = router;
