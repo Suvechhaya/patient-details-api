d6c4d3672fcd82f5583757b1013f45e705f69158
suvechhaya <suvechhaya@bajratechnologies.com>
2020-08-28 18:25:33 +0545

    Remove Joi validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,19 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema = {
-  name: Joi.string().required(),
-  email: Joi.string().required().email(),
-  password: Joi.string().min(8).required()
-};
-
 router.post('/register', async (req, res) => {
-
-  //Validating the data before we make a user
-  const validation = Joi.validate(req.body, schema);
-
-  // const {error} = Joi.validate(req.body, sch ema);
-
-//   const user = new User({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-//   try {
-//     const saveUser = await user.save();
-//   } catch (err) {
-//     res.status(400).send(err);
-//   }
+  // Create a new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
